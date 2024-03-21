92ea9e03769d20ec187c34e3f8cea33c3e4db119
migueldeoleiros <migueldeoleiros@gmail.com>
2024-03-21 12:09:14 +0100

    Move window focus script to scripts folder

---
+++ .config/hypr/scripts/hypr_focus_window.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Get a list of windows in hyprland and select one to move focus
+# Inspired to work similar to emacs buffers
+
+clients=$(hyprctl -j clients)
+
+window=$(echo "$clients" |
+    jq -r '.[] | select(.class != "" ) | "[\(.workspace.name)] \(.class) - \(.title)"' |
+    sort |
+    rofi -dmenu -i  -p "Select window")
+
+address=$(echo "$clients" |
+    jq -r --arg win "$window" '.[] | select(.class != "" and "[\(.workspace.name)] \(.class) - \(.title)" == $win) | .address')
+
+echo $window
+
+hyprctl dispatch focuswindow address:$address
