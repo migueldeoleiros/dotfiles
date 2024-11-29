44ed7a986eed81fe823e9bdcf4dd15433c260f7b
migueldeoleiros <migueldeoleiros@gmail.com>
2024-11-29 10:31:56 +0100

    Add script for screen mirror toggle

---
+++ .config/hypr/scripts/mirror_toggle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#migueldeoleiros: November 2024
+
+#simple script that toggles a commented line, in this case for switching between extending or sharing screen in hyprland
+
+# Set the file path and the line number to toggle
+FILE="/home/miguel/.config/hypr/hyprland.conf"
+LINE_NUMBER=5
+
+# Use sed to toggle the comment on the specified line
+if sed -n "${LINE_NUMBER}p" "$FILE" | grep -q '^#'; then
+  sed -i "${LINE_NUMBER}s/^#//" "$FILE"
+else
+  sed -i "${LINE_NUMBER}s/^/#/" "$FILE"
+fi
