93622c0fb099cda0c045ddcc0209354a5214a374
migueldeoleiros <miguel.lopez@merlinsoftware.es>
2025-04-20 11:53:44 +0200

    (hypr): fix monitor swap script

--- .config/hypr/scripts/swap_workspace_monitor.sh
+++ .config/hypr/scripts/swap_workspace_monitor.sh
@@ -6,18 +6,26 @@
 
 # Get the IDs of all monitors
 MONITORS=$(hyprctl monitors -j | jq -r '.[] | .id')
+echo "Monitors: $MONITORS"
 
 # Get the ID of the currently focused monitor
 CURRENT=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .id')
+echo "Current Monitor: $CURRENT"
 
 # Find the index of the current monitor in the list
 INDEX=$(echo "$MONITORS" | tr ' ' '\n' | grep -n "$CURRENT" | cut -d: -f1)
+echo "Index: $INDEX"
 
 # Calculate the index of the next monitor
-NEXT_INDEX=$(( (INDEX + 1) % $(echo "$MONITORS" | wc -w) ))
+NEXT_INDEX=$(( (INDEX) % $(echo "$MONITORS" | wc -w) ))
+echo "Next Index: $NEXT_INDEX"
 
 # Get the ID of the next monitor
 NEXT_MONITOR=$(echo "$MONITORS" | tr ' ' '\n' | head -n $((NEXT_INDEX + 1)) | tail -n 1)
+
+# NEXT_MONITOR=$(echo "$MONITORS" | tr ' ' '\n' | sed -n "${NEXT_INDEX}p")
+
+echo "Next Monitor: $NEXT_MONITOR"
 
 # Move the current workspace to the next monitor
 hyprctl dispatch movecurrentworkspacetomonitor "$NEXT_MONITOR"
